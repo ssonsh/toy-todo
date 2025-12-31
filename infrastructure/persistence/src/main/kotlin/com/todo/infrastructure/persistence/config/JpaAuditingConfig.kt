d4f2c4909727241b5ef11979cb8ef23eab326eda
ssh1224 <ssh1224@woowahan.com>
2025-12-31 18:22:43 +0900

    remove: infrastructure module

--- infrastructure/persistence/src/main/kotlin/com/todo/infrastructure/persistence/config/JpaAuditingConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.todo.infrastructure.persistence.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-
-@Configuration
-@EnableJpaAuditing
-class JpaAuditingConfig {
-}
--- infrastructure/persistence/src/main/kotlin/com/todo/infrastructure/persistence/member/MemberJpaRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.todo.infrastructure.persistence.member
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface MemberJpaRepository : JpaRepository<MemberJpaEntity, Long> {
-    fun existsByEmail(email: String): Boolean
-}
